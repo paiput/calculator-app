62c69259163bdc3fd4a0644882cce8784d22b2b4
Lucas Piputto <lucaspiputto@gmail.com>
2022-03-10 18:09:31 -0300

    First commit

---
+++ src/components/Display/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Display() {
+  return (
+    <div>
+      display
+    </div>
+  );
+}
+
+export default Display;
---
+++ src/components/ThemeSwitch/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ThemeSwitch() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default ThemeSwitch;

f07bb4b152db905cb99b9df2e04c07aea4b105fa
Lucas Piputto <lucaspiputto@gmail.com>
2022-03-11 16:51:58 -0300

    Add type to keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
-import "./App.css";
-import Button from "./components/Button";
+import { useState } from "react";
+
+// components
+import Header from "./components/Header";
+import Display from "./components/Display";
+import KeyPad from "./components/KeyPad";
+import Key from "./components/Key";
+import ThemeSwitch from "./components/ThemeSwitch";
+
+const DEFAULT_KEYS = [
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+  "+", "-", "x", "/", "."
+];
+const TEXT_KEYS = ["DEL", "RESET"];
+const SPECIAL_KEYS = ["="];
 
 function App() {
+  const [displayValue, setDisplayValue] = useState("");
+
   return (
     <div className="App">
-      hola
-      <Button>a</Button>
+      <Header>
+        <ThemeSwitch />
+      </Header>
+      <Display displayValue={displayValue} />
+      <KeyPad>
+        {DEFAULT_KEYS.map(character =>
+          <Key key={character} type="default" setDisplayValue={setDisplayValue}>
+            {character}
+          </Key>
+        )}
+        {TEXT_KEYS.map(character =>
+          <Key key={character} type="text" setDisplayValue={setDisplayValue}>
+            {character}
+          </Key>
+        )}
+        {SPECIAL_KEYS.map(character =>
+          <Key key={character} type="special" setDisplayValue={setDisplayValue}>
+            {character}
+          </Key>
+        )}
+      </KeyPad>
     </div>
   );
 }

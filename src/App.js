d77c7d8d4eaa0f56e764501a697d6525ee8c823b
Lucas Piputto <lucaspiputto@gmail.com>
2022-03-24 19:57:29 -0300

    Add operations

--- src/App.js
+++ src/App.js
@@ -12,14 +12,18 @@ const OPERANDS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function App() {
   const [displayValue, setDisplayValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [operator, setOperator] = useState("");
+  const [error, setError] = useState("");
   const [theme, setTheme] = useState("theme-1");
 
   useEffect(() => {
     setTheme(localStorage.getItem("theme"));
   }, []);
 
-  const appendNumber = (number) => {
-    setDisplayValue(prevValue => prevValue + number);
+  const appendDigit = (digit) => {
+    if (digit === "." && displayValue.includes(digit)) return;
+    setDisplayValue(prevValue => prevValue + digit);
   };
 
   const deleteNumber = () => {
@@ -28,44 +32,83 @@ function App() {
 
   const resetDisplay = () => {
     setDisplayValue("");
+    setInputValue("");
+    setOperator("");
+    setError("");
   };
 
+  const handleOperation = (selectedOperator) => {
+    if (operator) {
+      setOperator(selectedOperator);
+      setDisplayValue("");
+      return;
+    }
+    setOperator(selectedOperator);
+    setInputValue(displayValue);
+    setDisplayValue("");
+  };
+  
   const calcResult = () => {
-    //...
+    if (!inputValue && !displayValue && operator) {
+      setError("Error: syntax error");
+    }
+
+    let result;
+
+    if (operator === "+") {
+      result = Number(inputValue) + Number(displayValue);
+    } else if (operator === "-") {
+      result = Number(inputValue) - Number(displayValue);
+    } else if (operator === "x") {
+      result = Number(inputValue) * Number(displayValue);
+    } else if (operator === "/") {
+      result = Number(inputValue) / Number(displayValue);
+    } else return;
+
+    if (result === Infinity || isNaN(result)) {
+      setError("Error: math error");
+    }
+
+    setInputValue("" + result);
   };
 
   return (
-    <div className="App" data-theme={theme}>
+    <div className="calculator" data-theme={theme}>
       <Header>
         <ThemeSwitch setTheme={setTheme} />
       </Header>
-      <Display displayValue={displayValue} />
+      <Display 
+        displayValue={displayValue} 
+        inputValue={inputValue} 
+        operator={operator}
+        error={error}
+      />
       <KeyPad>
         {OPERANDS.map(character => 
-          <Key key={character} id={`key-${character}`} handleClick={appendNumber}>
+          <Key key={character} id={`key-${character}`} handleClick={appendDigit}>
             {character}
           </Key>  
         )}
-        <Key id="key-sum">
+        <Key id="key-sum" handleClick={handleOperation}>
           +
         </Key>
-        <Key id="key-subtraction">
+        <Key id="key-subtraction" handleClick={handleOperation}>
           -
         </Key>
-        <Key id="key-multiplication">
+        <Key id="key-multiplication" handleClick={handleOperation}>
           x
         </Key>
-        <Key id="key-division">
+        <Key id="key-division" handleClick={handleOperation}>
           /
         </Key>
-        <Key id="key-dot">
+        <Key id="key-dot" handleClick={appendDigit}>
           .
         </Key>
         <Key id="key-delete" type="text" handleClick={deleteNumber}>
-          DEL
+          del
         </Key>
         <Key id="key-reset" type="text" large handleClick={resetDisplay}>
-          RESET
+          reset
         </Key>
         <Key id="key-equals" type="special" large handleClick={calcResult}>
           =

e094c524b53439685d2c9d382633c3cea89385db
Lucas Piputto <lucaspiputto@gmail.com>
2022-03-24 16:31:02 -0300

    Show input value on display

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./display.module.css";
 
-function Display({ displayValue }) {
+function Display({ displayValue, inputValue, operator }) {
+  const numberWithCommas = (number) => {
+    // make it not add the commas to numbers after a dot.
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   return (
     <section className={styles.display}>
-      <span>
-        {displayValue}
+      <span className={styles.inputValue}>
+        {numberWithCommas(inputValue)} {operator}
+      </span>
+      <span className={styles.displayValue}>
+        {numberWithCommas(displayValue)}
       </span>
     </section>
   );
 }
 
 Display.propTypes = {
-  displayValue: PropTypes.string.isRequired
+  displayValue: PropTypes.string.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  operator: PropTypes.string.isRequired
 };
 
 export default Display;

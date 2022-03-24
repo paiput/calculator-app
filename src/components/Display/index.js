d5f7ba1fa7b4c60190c03fbd204b8ac870df39bc
Lucas Piputto <lucaspiputto@gmail.com>
2022-03-24 19:07:24 -0300

    Refactor Display component
    Also add horizontall scrollbar

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -2,20 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./display.module.css";
 
-function Display({ displayValue, inputValue, operator }) {
+function Display({ displayValue, inputValue, operator, error }) {
   const numberWithCommas = (number) => {
     // make it not add the commas to numbers after a dot.
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
   return (
-    <section className={styles.display}>
-      <span className={styles.inputValue}>
-        {numberWithCommas(inputValue)} {operator}
-      </span>
-      <span className={styles.displayValue}>
-        {numberWithCommas(displayValue)}
-      </span>
+    <section className={`${styles.display} ${operator ? styles.reduced : ""}`}>
+      {error 
+        ? <span className={styles.errorMsg}>{error}</span>
+        : <>
+          <span className={styles.inputValue}>
+            {numberWithCommas(inputValue)} {operator}
+          </span>
+          <span className={styles.displayValue}>
+            {numberWithCommas(displayValue)}
+          </span>
+        </>}
     </section>
   );
 }
@@ -23,7 +27,8 @@ function Display({ displayValue, inputValue, operator }) {
 Display.propTypes = {
   displayValue: PropTypes.string.isRequired,
   inputValue: PropTypes.string.isRequired,
-  operator: PropTypes.string.isRequired
+  operator: PropTypes.string.isRequired,
+  error: PropTypes.string.isRequired
 };
 
 export default Display;
